f5357ef8fd40552b582a42dc1f25b872b8498a50
晓晓白熊 <1603018187@qq.com>
2017-01-04 16:52:27 +0000

    最终版本1.10

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,6 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
   #host = 'https://courseselect02-a18366182849.cs50.io'
-  host = 'https://mailpanduan.herokuapp.com'
+  host = 'https://group-work.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
 end
